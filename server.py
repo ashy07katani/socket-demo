a4e31d8fc0aef8fca2bd1de96188f9cf00143d42
ashy07katani <tripathi.ashish29@gmail.com>
2022-09-30 16:01:34 +0530

    buffering

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 from encodings import utf_8
 import socket
+import random
+import string
+HEADER_SIZE = 10
+
 def createSocket():
     #this method takes two arguments first if ipv4 /v6 and tcp/udp . By default ipv4 and tcp
     serverSocket  = socket.socket()
     print(serverSocket)
     #next thing is bind some socket passing ip address and port as a single object
-    serverSocket.bind(("192.168.93.9",9999))
+    serverSocket.bind(('localhost',9999))
     #Listen passively for the connection, important thing here is backlog which is a queue, all the request will come the queue if reuest exceed the size of the queue then it is simply refused
     serverSocket.listen(3)
     print("server is Listening")
@@ -14,9 +18,16 @@ def createSocket():
     # we want to accept in a continuous loop
     while(True):
         #this mehtod would return the client socket and address of it
+        size = random.randint(100,1001)
+        # generating random strings
+        res = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k=size))
         clientSocket,address=serverSocket.accept()
-        name= clientSocket.recv(1024).decode('utf-8')
-        print("connection with",address,name)
-        clientSocket.send(bytes('welcome to the chat room','utf-8'))
-        clientSocket.close()
+        msg = res
+        msg=f"{len(msg):<{HEADER_SIZE}}"+msg
+        #'{:<30}'.format('left aligned')
+        #name= clientSocket.recv(1024).decode('utf-8')
+        print("connection with",address)
+        clientSocket.send(bytes(msg,'utf-8'))
+        print("data sent ",msg)
 createSocket()
